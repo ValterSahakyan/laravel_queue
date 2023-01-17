32cd31d8bb43f7c4d4471f8dbbc0edbd40acf0f7
Valter Sahakyan <valtersahakyan92@gmail.com>
2023-01-17 20:36:14 +0400

    initial

---
+++ app/Http/Controllers/FibonacciController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Jobs\CountFibonacci;
+use Illuminate\Support\Facades\Validator;
+
+class FibonacciController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'title' => 'required|string|max:225',
+                'body' => 'required',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return $validator->errors()->all();
+        }
+
+        $validatedData = $validator->validated();
+
+        CountFibonacci::dispatch($validatedData);
+        return response()->json(['message' => 'post has been sent']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
